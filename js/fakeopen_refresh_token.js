237fa78870fb3c614ee8c583d16106937f3aba8c
SLCA <sun2589986171@gmail.com>
2023-10-27 18:52:46 +0800

    合并刷新接口，调整存储结果为 JSON

--- js/fakeopen_refresh_token.js
+++ js/fakeopen_refresh_token.js
@@ -6,31 +6,58 @@ const got = require("got");
 const fs = require("fs");
 const path = require("path");
 
-const resultPath = path.join(__dirname, "result.txt");
+const resultPath = path.join(__dirname, "result.json");
+const resultErrPath = path.join(__dirname, "result_err.json");
 const authJsonPath = path.join(__dirname, "auth.json");
-
 const refreshtokenList = JSON.parse(fs.readFileSync(authJsonPath)); // 读取 auth.json 文件
 
-let resStr = "";
-let currentRefreshtoken = "";
+let resJSON = [];
+let resItem = {
+    username: "",
+    password: "",
+    refresh_token: "",
+    session_token: "",
+    fk_token: "",
+    login_time: "",
+};
 
 //脚本入口函数main()
 async function main() {
     for (let index = 0; index < refreshtokenList.length; index++) {
-        const item = refreshtokenList[index];
-        currentRefreshtoken = item.refresh_token;
+        const { username, password, refresh_token, session_token } =
+            refreshtokenList[index];
 
-        resStr += `${item.email},${item.password}\n`;
+        resItem.username = username;
+        resItem.password = password;
+        resItem.refresh_token = refresh_token;
+        resItem.session_token = "";
+        resItem.fk_token = "";
+        resItem.login_time = new Date().toLocaleString();
 
-        //开始账号任务
-        await refreshToken(currentRefreshtoken, index + 1, item.email);
+        // 判断是否有 refresh_token
+        if (refresh_token && refresh_token !== "") {
+            //开始账号任务
+            await refreshToken(refresh_token, index + 1, username);
+        } else {
+            //开始账号任务
+            await refreshTokenBySession(session_token, index + 1, username);
+        }
+
+        resJSON.push(JSON.parse(JSON.stringify(resItem)));
     }
 
-    fs.writeFileSync(resultPath, resStr); // 存储结果
-    console.log(`\n最终结果已生成在 result.txt\n`);
+    fs.writeFileSync(
+        resultPath,
+        JSON.stringify(resJSON.filter((item) => item.fk_token !== ""))
+    ); // 存储结果
+    fs.writeFileSync(
+        resultErrPath,
+        JSON.stringify(resJSON.filter((item) => item.fk_token === ""))
+    ); // 存储结果
+    console.log(`\n最终结果已生成在 result.json\n`);
 }
 
-// 刷新Token接口
+// 刷新Token接口通过 refresh_token
 async function refreshToken(token, index, email) {
     console.log(`开始刷新账号[${index}]: ${email}`);
     try {
@@ -55,8 +82,35 @@ async function refreshToken(token, index, email) {
     } catch (e) {
         //打印错误信息
         console.log(e);
+    }
+}
 
-        resStr += `${e}\n\n`;
+// 刷新Token接口通过 session_token
+async function refreshTokenBySession(token, index, email) {
+    console.log(`开始刷新账号[${index}]: ${email}`);
+    try {
+        let urlObject = {
+            fn: "/auth/session",
+            method: "post",
+            url: "https://ai.fakeopen.com/auth/session",
+            form: {
+                session_token: token,
+            },
+            //超时设置
+            timeout: 15000,
+        };
+
+        const { result } = await request(urlObject);
+
+        if (result.access_token && result.access_token !== "") {
+            resItem.session_token = result.session_token || "";
+            await tokenRegister(result.access_token, index);
+        } else {
+            console.log(`账号[${index}]\n${JSON.stringify(result)}`);
+        }
+    } catch (e) {
+        //打印错误信息
+        console.log(e);
     }
 }
 
@@ -81,8 +135,7 @@ async function tokenRegister(accessToken, index) {
         const { result } = await request(urlObject);
 
         if (result.token_key && result.token_key !== "") {
-            resStr += `${result.token_key}\n`;
-            resStr += `${currentRefreshtoken}\n\n`;
+            resItem.fk_token = result.token_key;
             console.log(`账号[${index}]成功`);
         } else {
             console.log(`账号[${index}]\n${JSON.stringify(result)}`);
