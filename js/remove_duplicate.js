d9fb356a5bffe8cef5220ed6e1642ba23a20e7ca
SLCA <sun2589986171@gmail.com>
2023-10-31 11:08:05 +0800

    优化去重

--- js/remove_duplicate.js
+++ js/remove_duplicate.js
@@ -6,12 +6,12 @@ const authJsonPath = path.join(__dirname, "auth.json");
 const refreshtokenList = JSON.parse(fs.readFileSync(authJsonPath)); // 读取 auth.json 文件
 
 // 对读取到的 auth.json 文件进行去重, 并将去重后的结果写入 result.json 文件
-fs.writeFileSync(
-    resultPath,
-    JSON.stringify(
-        refreshtokenList.filter(
-            (item, index, arr) =>
-                arr.findIndex((v) => v.username === item.username) === index
-        )
-    )
-);
+let uniqueMap = new Map();
+refreshtokenList.forEach((item) => {
+    if (!uniqueMap.has(item.username)) {
+        uniqueMap.set(item.username, item);
+    }
+});
+let uniqueList = [...uniqueMap.values()];
+
+fs.writeFileSync(resultPath, JSON.stringify(uniqueList));
