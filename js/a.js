ff76179e9b512e63a067f4b88233f444c04bcc83
SLCA <sun2589986171@gmail.com>
2023-11-14 16:42:20 +0800

    update

--- js/a.js
+++ js/a.js
@@ -2,12 +2,6 @@ const data = []
 
 let str = ''
 
-// 通过 username 对 data 去重，并返回去重后的数组
-function unique(arr) {
-  const res = new Map()
-  return arr.filter((a) => !res.has(a[0].username) && res.set(a[0].username, 1))
-}
-
 const arr = unique(data)
 
 // 提取账号密码
